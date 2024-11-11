f3505ea31c9866a6008cbbfd5f71a2ffff21c531
Govind Kailas <gkailas@fortinet.com>
2024-11-11 12:16:46 -0800

    respond in json

--- simple-app.go
+++ simple-app.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Node Name: %s\nPod Name: %s\nPod Namespace: %s\n", os.Getenv("NODE_NAME"), os.Getenv("POD_NAME"), os.Getenv("POD_NAMESPACE"))
+		data := map[string]string{
+			"nodeName":     os.Getenv("NODE_NAME"),
+			"podName":      os.Getenv("POD_NAME"),
+			"podNamespace": os.Getenv("POD_NAMESPACE"),
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
 	})
 
 	http.ListenAndServe(":80", nil)
