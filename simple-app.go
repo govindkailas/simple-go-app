c6cc4a68607fe405f88dca622b00dbfedaad5a95
Govind Kailas <gkailas@fortinet.com>
2024-11-11 12:56:23 -0800

    testing json format

--- simple-app.go
+++ simple-app.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]string{
+		json := map[string]string{
 			"nodeName":     os.Getenv("NODE_NAME"),
 			"podName":      os.Getenv("POD_NAME"),
 			"podNamespace": os.Getenv("POD_NAMESPACE"),
 		}
-
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(data)
+		fmt.Fprintf(w, `{"nodeName":"%s","podName":"%s","podNamespace":"%s"}`, json["nodeName"], json["podName"], json["podNamespace"])
 	})
 
 	http.ListenAndServe(":80", nil)
